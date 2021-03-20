4e27e8463abb62ba2b52f571888e89efb6850505
Thomas Whittle <tewhittle@gmail.com>
2021-03-20 10:21:41 -0400

    Establish database connection

---
+++ server.js
@@ -0,0 +1,34 @@
+const path = require("path");
+require("dotenv").config()
+const mysql = require("mysql")
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    port: 3306,
+    user: process.env.USER,
+    password: process.env.PASS,
+    database: "quotes_db"
+})
+
+connection.connect((err) => {
+    if (err) throw err
+    console.log("Connected")
+})
+
+app.get("/", (req, res) => {
+    console.log("Test")
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`)
+})
