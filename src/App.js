c950fc33439e79a4a8a94c857233726f99eb2847
Abdinajib Liban <abdinajibliban@gmail.com>
2021-03-20 12:16:37 -0400

    built login, add logo, import fragments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { Fragment } from 'react';
 import './App.css';
+import companyLogo from './firstpaint.jpg';
 
 function App() {
   return (
-    <h1>Paint Bid App</h1>
+    <Fragment>
+        
+        <div>
+          <h1 class='tc'> Paint Bid App</h1>
+          <img class=' w-100 ' src={companyLogo}/>
+        </div>
+
+        
+        <h1 class="tc">Sign in</h1> 
+        <form action="/action_page.php" class="tc">
+          <label for="email">Email: </label>
+          <input type="email" id="email" name="email"/> <br/><br/>
+          <label for="password">Password: </label>
+          <input type="password" id="password" name="password"/> <br/><br/>
+          <input type="submit" value="Submit"/>
+        </form>
+    
+
+
+    </Fragment>
+    
+  
+ 
+    
   );
 }
 
