0d1c660547737fbf1509ff85dd3833db3073cdd9
Thomas Whittle <tewhittle@gmail.com>
2021-03-20 12:21:53 -0400

    Add sequelize model for paint

---
+++ models/Paint.js
@@ -0,0 +1,27 @@
+module.exports = ( sequelize, DataTypes ) => {
+    const Paint = sequelize.define("Paint", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        quality: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        pricePerGal: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        }
+    })
+    return Paint
+}
