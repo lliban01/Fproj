cdc08af6ab6e3804db611e7f5ca65e8c97bf1cbc
Thomas Whittle <tewhittle@gmail.com>
2021-03-20 11:24:44 -0400

    Add client model

---
+++ models/Client.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+    const Client = sequelize.define("Client", {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        fullName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        email: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        }
+    })
+    return Client;
+}
