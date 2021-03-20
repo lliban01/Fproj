78a6f61eada3295c7012b36d608f0af34fc9164e
Thomas Whittle <tewhittle@gmail.com>
2021-03-20 12:15:54 -0400

    Add sequelize model for orders

---
+++ models/Order.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+    const Order = sequelize.define("Order", {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            validate: {
+                notEmpty: true
+            }
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        finalQuote: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        clientId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        }
+    })
+    return Order;
+}
