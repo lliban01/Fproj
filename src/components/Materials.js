7df681d5293eb3441a0314ffcba27b10efba0d7c
Abdinajib Liban <abdinajibliban@gmail.com>
2021-03-25 19:03:44 -0400

    added initial Materials component text

--- src/components/Materials.js
+++ src/components/Materials.js
@@ -1,1 +1,12 @@
-//Materials comp
+import React from 'react';
+
+function Materials() {
+   return (
+       <div>
+           <h1>Materials</h1>   
+       </div>
+   )
+}
+
+export default Materials;
+
