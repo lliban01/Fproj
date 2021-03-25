7468730057be37514b783ffab078f9c5c87b05c4
Abdinajib Liban <abdinajibliban@gmail.com>
2021-03-25 19:20:00 -0400

    added imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Main from "./components/Main";
+import Labor from "./components/Labor";
+import Quote from "./components/Quote";
+import Newbids from "./components/Newbids";
+import Paint from "./components/Paint";
+import Materials from "./components/Materials";
+import Oldbids from './components/Oldbids';
 
 function App() {
   return (
