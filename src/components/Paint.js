9d65e6896c2ae7565f0f7cc0b9c75cfa2fc4875f
Abdinajib Liban <abdinajibliban@gmail.com>
2021-03-27 10:36:59 -0400

    paint quality dropdown, paint notes input, added sq ft box and working buttons

--- src/components/Paint.js
+++ src/components/Paint.js
@@ -3,11 +3,25 @@ import { Link } from "react-router-dom";
 
 
 function Paint() {
-   return (
-       <div>
-           <h1>Paint</h1>
-       </div>
-   )
-}
-
-export default Paint;
+    return (
+        <div>
+            <h1>Paint Quality</h1>
+            <select  name="Paint Quality">
+                 <option value="Affordable"> Affordable </option>
+                 <option value="Standard">Standard</option>
+                 <option value="Premium">Premium</option>
+             </select><br/><br/>
+             <h1>Paint Notes</h1> 
+             <textarea name="text" cols="25" rows="5" placeholder="Add text here!!"></textarea><br/><br/>
+             <h1> Sq Ft of House</h1>
+             <input placeholder="1500 sq ft"></input> <br/><br/>
+ 
+             <div>
+                 <button> <Link to="/newbids"> Back</Link></button>
+                 <button> <Link to="/materials"> Materials</Link></button>
+             </div>
+        </div>
+    )
+ }
+ 
+ export default Paint;
