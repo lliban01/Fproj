acf590470170b664b5d23e6b20f0b129390b0462
Abdinajib Liban <abdinajibliban@gmail.com>
2021-03-27 10:39:36 -0400

    quote page, added final costs fields, working buttons

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,12 +2,27 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 function Quote() {
-   return (
-       <div>
-           <h1>Quote</h1>
-       </div>
-   )
-   
-}
-
-export default Quote;
+    return (
+        <div>
+             <h1>Quote</h1>
+             <label for="FinalPaintCost"> Final Paint Cost <span>$</span></label>
+                 <input value="1000.00"/><br/>
+             <label for="FinalMaterialsCost"> Final Materials Cost <span>$</span></label>
+                 <input value="1000.00"/><br/>
+             <label for="FinalLaborCost"> Final Labor Cost <span>$</span></label>
+                 <input value="1000.00"/><br/>
+             <label for="Markup"> Mark-up (Profit) <span>$</span></label>
+                 <input value="1000.00"/><br/>
+ 
+             <h3>Final Quote :  <span>$4,000.00</span> </h3> <br/><br/>
+             <div>
+                 <button> <Link to="/labor"> Back</Link></button>
+                 <button> <Link to="/main"> Finish</Link></button>
+             </div>
+            
+        </div>
+    )
+    
+ }
+ 
+ export default Quote;
