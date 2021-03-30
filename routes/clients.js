d682019b4cf2a9e2d4d00c69630401d669b83c3b
Thomas Whittle <tewhittle@gmail.com>
2021-03-30 14:59:03 -0400

    Add routes to client.js

--- routes/clients.js
+++ routes/clients.js
@@ -1,1 +1,44 @@
 const db = require("../models")
+
+module.exports = (app) => {
+    app.get("/api/clients/", (req, res) => {
+        db.Client.findAll({})
+        .then((data) => res.json(data))
+    })
+
+    app.get("/api/clients/:id", (req, res) => {
+        db.Client.findAll({
+            where: {
+                id: req.params.id
+            }
+        }).then((data) => {
+            res.json(data)
+        })
+    })
+    
+    app.post("/api/clients/", (req, res) => {
+        console.log(req.body)
+        db.Client.create({
+            fullName: req.body.fullName,
+            address: req.body.address,
+            email: req.body.email
+        })
+        .catch((err) => {
+            if (err) {
+                console.log(err)
+            }
+        })
+        .then((data) => res.json(data))
+        
+    })
+    
+    app.delete("/api/clients/:id", (req, res) => {
+        db.Client.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then((data) => { 
+            res.json(data)
+        })
+    })
+}
